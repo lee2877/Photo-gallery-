adf044d893b0d1530b11433ac75940ab89fa3b6e
Haeun Lee <haeun930515@gmail.com>
2019-05-23 18:17:01 -0400

    Include editor api and tool box

--- jsForEdit.js
+++ jsForEdit.js
@@ -1,1 +1,28 @@
-sdfa
+document.addEventListener('DOMContentLoaded', function(){
+    var loadImage = document.getElementById('load');
+    function loadInputHandler(event) {
+        var imageFile = event.target.files[0];
+        var imageElement = document.getElementById('image');
+        imageElement.setAttribute('src', URL.createObjectURL(imageFile));
+    };
+    loadImage.onchange = loadInputHandler;
+
+    function changeSliderHandler(event){
+        Caman("#image", function readerCaman() {
+            this.revert(false);
+            this[event.target.name](event.target.value).render();
+        });
+    };
+    var brightnessRange = document.getElementById("brightness");
+    brightnessRange.onchange = changeSliderHandler;
+
+    var vibranceRange = document.getElementById("vibrance");
+    vibranceRange.onchange = changeSliderHander;
+
+    var hueRange = docunemt.getElementById("hue");
+    hueRange.onchange = changeSliderHandler;
+
+    var gammaRange = document.getElementById("gamma");
+    gammaRange.onchange = changeSliderHandler;
+    
+}, false);
